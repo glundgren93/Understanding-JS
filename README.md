04aad0dbfb723daf0605bc9fdd8252f053d82a4f
glundgren93 <glundgren93@gmail.com>
2016-03-27 14:21:07 -0300

    adding underscore.js

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Functional-JS
-Functional JS for study purposes
+
+This repository is meant for study purposes only.
+Most of the code of it will come from the book Functional JavaScript by Michael Fogus
+
+# What is Functional Programming?
+  "Functional programming is the use of functions that transform values into units of abstraction, subsequently used to build software systems."
+
+# Why Functional Programming matters?
+  "The major evolution that is still going on for me is towards a more functional programming style, which involves unlearning a lot of old habits, and backing away from some OOP directions." - John Carmack
---
+++ app.js
@@ -0,0 +1,32 @@
+// All functions have access to the apply method.
+// This method allows you to call the function with an array.
+// The array would be the argument to the function itself.
+
+function splat(fn) {
+  return function(array) {
+    // The elements of the array will be passed as arguments to fn
+    return fn.apply(null, array);
+  };
+}
+
+var addElements = splat(function(x, y, z) {
+  return x + y + z;
+});
+
+console.log(addElements([1, 2, 3]));
+
+// Any function may be called with any number of arguments of any type
+function unsplat(fn) {
+  // Although this function doesn't explicitly have arguments, it can receive as many arguments as you wish
+  return function() {
+    // All functions have access to arguments property,
+    // that is an array-like structure holding the values that the function was called with.
+    return fn.call(null, _.toArray(arguments));
+  };
+}
+
+var joinElements = unsplat(function(array) {
+  return array.join(' ');
+});
+
+console.log(joinElements(1, 2, 3));
