9981c5384eb0ed189732baa06ad31bb7fc77d847
glundgren93 <glundgren93@gmail.com>
2016-04-26 22:09:41 -0300

    adding purity

---
+++ src/purity.js
@@ -0,0 +1,19 @@
+/* Purity
+
+ One way of reducing the complexity of softwares is reducing or
+ eliminating state change in our programs.
+
+ A pure function contains the following properties:
+  - Its result is calculated only from the values of its arguments.
+  - It cannot rely on data that changes external to is control.
+  - It cannot change the state of something external to its body.
+
+Given the same arguments, pure functions always return the same output.
+
+Pure functions are important for:
+ - Functional programming
+ - Concurrency
+ - Writing testable code
+ - Having predictable code
+
+*/
